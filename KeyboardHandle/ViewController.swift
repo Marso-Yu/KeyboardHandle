140f8adddcbd67758ac70c78604935fa85ceef2d
MarsoYu <marso.yu777@gmail.com>
2021-05-27 09:31:29 +0800

    Re git push

--- KeyboardHandle/ViewController.swift
+++ KeyboardHandle/ViewController.swift
@@ -8,12 +8,70 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var accountTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var blueViewBottomConstraint: NSLayoutConstraint!
+    
+    var moveHeight: CGFloat?
+    var textFieldTargetY: CGFloat?
+    var keyboardShowStatus: Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        NotificationCenter.default.addObserver(self,
+        selector: #selector(keyboardWillShow),
+        name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        NotificationCenter.default.addObserver(self,
+        selector: #selector (keyboardWillHide),
+        name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+        accountTextField.delegate = self
+        passwordTextField.delegate = self
+        
     }
 
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue
+        {
+            let keyboardY = self.view.bounds.size.height - keyboardSize.height
+            
+            if keyboardShowStatus == true{
+                return
+            }
 
+            keyboardShowStatus = true
+                
+            if let textFieldTargetY = textFieldTargetY {
+                if textFieldTargetY > keyboardY {
+                    moveHeight = textFieldTargetY - keyboardY
+                    blueViewBottomConstraint.constant += moveHeight ?? 0
+                }
+            }
+        }
+    }
+    
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        blueViewBottomConstraint.constant -= moveHeight ?? 0
+        keyboardShowStatus = false
+    }
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+            self.view.endEditing(true)
+    }
 }
 
+extension ViewController: UITextFieldDelegate {
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        let textFieldRect = textField.convert(textField.bounds, to:self.view)
+        textFieldTargetY = textFieldRect.origin.y + textFieldRect.size.height
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
